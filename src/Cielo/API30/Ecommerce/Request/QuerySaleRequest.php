e7ad56cb760d6385b6561b85fce0eea9e7f4fb61
Eduardo Roseo <eduardo.roseo@me2.com.br>
2023-07-24 14:50:02 -0300

    feat: iniciando projeto

---
+++ src/Cielo/API30/Ecommerce/Request/QuerySaleRequest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Cielo\API30\Ecommerce\Request;
+
+use Cielo\API30\Ecommerce\Sale;
+use Cielo\API30\Environment;
+use Cielo\API30\Merchant;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class QuerySaleRequest
+ *
+ * @package Cielo\API30\Ecommerce\Request
+ */
+class QuerySaleRequest extends AbstractRequest
+{
+
+    private $environment;
+
+	/**
+	 * QuerySaleRequest constructor.
+	 *
+	 * @param Merchant $merchant
+	 * @param Environment $environment
+	 * @param LoggerInterface|null $logger
+	 */
+    public function __construct(Merchant $merchant, Environment $environment, LoggerInterface $logger = null)
+    {
+        parent::__construct($merchant, $logger);
+
+        $this->environment = $environment;
+    }
+
+    /**
+     * @param $paymentId
+     *
+     * @return null
+     * @throws \Cielo\API30\Ecommerce\Request\CieloRequestException
+     * @throws \RuntimeException
+     */
+    public function execute($paymentId)
+    {
+        $url = $this->environment->getApiQueryURL() . '1/sales/' . $paymentId;
+
+        return $this->sendRequest('GET', $url);
+    }
+
+    /**
+     * @param $json
+     *
+     * @return Sale
+     */
+    protected function unserialize($json)
+    {
+        return Sale::fromJson($json);
+    }
+}
---
+++ src/Cielo/API30/Ecommerce/Request/TokenizeCardRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Cielo\API30\Ecommerce\Request;
+
+use Cielo\API30\Ecommerce\CreditCard;
+use Cielo\API30\Ecommerce\Environment;
+use Cielo\API30\Merchant;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Class CreateCardTokenRequestHandler
+ *
+ * @package AppBundle\Handler\Cielo
+ */
+class TokenizeCardRequest extends AbstractRequest
+{
+
+    private $environment;
+    /** @var Merchant $merchant */
+    private $merchant;
+
+	/**
+	 * CreateCardTokenRequestHandler constructor.
+	 *
+	 * @param Merchant $merchant
+	 * @param Environment $environment
+	 * @param LoggerInterface|null $logger
+	 */
+    public function __construct(Merchant $merchant, Environment $environment, LoggerInterface $logger = null)
+    {
+        parent::__construct($merchant, $logger);
+
+        $this->merchant    = $merchant;
+        $this->environment = $environment;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function execute($param)
+    {
+        $url = $this->environment->getApiUrl() . '1/card/';
+
+        return $this->sendRequest('POST', $url, $param);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function unserialize($json)
+    {
+        return CreditCard::fromJson($json);
+    }
+}
